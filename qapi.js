d67c9985f976168b94db54eac3b68c877e74b7ea
qptf <jack_okira@yahoo.com>
2020-11-07 22:47:39 +0700

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa6b932badc03001fb09d00",
+				"5fa6b932badc03001fb09d02"
+			]
+		},
+		"5fa6b932badc03001fb09d00": {
+			"id": "5fa6b932badc03001fb09d00",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa6b932badc03001fb09d02": {
+			"id": "5fa6b932badc03001fb09d02",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
